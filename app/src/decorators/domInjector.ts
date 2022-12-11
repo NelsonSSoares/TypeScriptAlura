d879d8f45037b5b2e23365ea97b92c0868e0e3c4
Nelson de Sousa <punkpunkada@gmail.com>
2022-12-11 20:31:51 -0200

    cache decorator

--- app/src/decorators/domInjector.ts
+++ app/src/decorators/domInjector.ts
@@ -4,16 +4,23 @@ export function domInjector(selector: string){
         console.log(`buscando prototype ${target.constructor.name}
          e adiocnando getter para a propriedade ${propertyKey}`);
         
+        let elemento: HTMLElement | null = null;
 
         const getter = function (){
+         if(!elemento){
+            elemento = <HTMLElement> document.querySelector(selector);
+         }   
 
-            const elemento = document.querySelector(selector);
+        
+            
             console.log(`buscando elemento do DOM com o seletor: ${selector} para injetar em: ${propertyKey} `);
             
             return elemento;
+        
         }
 
-        Object.defineProperty(target, propertyKey, {
+        Object.defineProperty(target, propertyKey,
+        {
             get: getter
         });
     }
