c3f0990f45653e09b755cdf5752d24233ec4785c
Nelson <punkpunkada@gmail.com>
2022-12-12 17:32:12 -0300

    API

--- dist/js/src/decorators/domInjector.js
+++ dist/js/src/decorators/domInjector.js
@@ -2,8 +2,11 @@ export function domInjector(selector) {
     return function (target, propertyKey) {
         console.log(`buscando prototype ${target.constructor.name}
          e adiocnando getter para a propriedade ${propertyKey}`);
+        let elemento = null;
         const getter = function () {
-            const elemento = document.querySelector(selector);
+            if (!elemento) {
+                elemento = document.querySelector(selector);
+            }
             console.log(`buscando elemento do DOM com o seletor: ${selector} para injetar em: ${propertyKey} `);
             return elemento;
         };
